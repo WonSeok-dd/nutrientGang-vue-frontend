0d34b543104baa4e2b88510f85ee11037cb23ad7
WonSeok-dd <cluejws@gmail.com>
2022-10-04 04:10:29 +0900

    fix: 음식점 등록 기능 수정

--- src/layouts/register/RegisterRestaurant.vue
+++ src/layouts/register/RegisterRestaurant.vue
@@ -13,6 +13,8 @@
                 <ValidationObserver ref="observer" v-slot="{invalid}">
                     <v-form @submit.prevent="submit">
                         
+
+                        <!--음식점 이름-->
                         <v-card elevation="10" outlined>
                             <v-card-title class="text--primary font-weight-black">음식점 이름</v-card-title>
                             <v-card-text>
@@ -28,36 +30,74 @@
 
                         <v-divider class="mt-10 mb-10"></v-divider>
 
-                        <v-card elevation="10" outlined>
-                            <v-card-title class="text--primary font-weight-black">음식점 위치</v-card-title>
 
+                        <!--음식점 주소-->
+                        <v-card elevation="10" outlined :class="{errorState : rtrLocationCheck === false, successState : rtrLocationCheck === true }">
+                            <v-card-title class="text--primary font-weight-black">음식점 주소</v-card-title>
+                            
+                            <!--음식점 주소 입력-->
                             <v-card-text>
                                 <ValidationProvider :rules="{
                                     required : true,
-                                    }" name="음식점 위치" v-slot="{errors}">
-                                    <v-text-field v-model="rtrLocation" label="음식점 위치" :error-messages="errors"
+                                    address : true,
+                                    }" name="음식점 주소" v-slot="{errors}">
+                                    <v-text-field v-model="rtrLocation" label="음식점 주소" :error-messages="errors"
                                     prepend-icon="mdi-map-marker" clearable class="mt-4"
                                     ></v-text-field>
                                 </ValidationProvider>
                             </v-card-text>
                             
-                            <v-card-text>{{answer}}</v-card-text>
 
-                            <v-card-text>
-                                <v-btn color="primary" text @click="checkRtrLocation">확인</v-btn>
-                            </v-card-text> 
+                            <!--음식점 주소 다이얼로그 및 주소정보확인 버튼-->
+                            <v-dialog v-model="rtrLocationdialog" width="500">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-row justify="center" class="pd-5">
+                                        
+                                        <!--주소정보확인 메시지-->
+                                        <v-col cols="auto">
+                                            <v-alert v-if="rtrLocationCheck" dense outlined type="success">해당 주소 정보는 올바른 위치 정보입니다.</v-alert>
+                                            <v-alert v-else dense outlined type="error">해당 주소 정보는 올바른 위치 정보가 아닙니다.</v-alert>
+                                        </v-col>
+                                        
+                                        <!--주소정보확인 버튼-->
+                                        <v-col cols="auto">
+                                            <v-alert color="success" dark dense v-bind="attrs" v-on="on" @click="checkRtrLocation"> 
+                                                <v-icon left>mdi-map-marker-radius</v-icon>주소 정보 확인
+                                            </v-alert>
+                                        </v-col>
+                                    </v-row>
+                                </template>
 
+                                <!--음식점 주소 다이얼로그-->
+                                <v-card>
+                                  <v-alert v-if="rtrLocationCheck" dense outlined type="success">해당 <strong>주소 정보</strong>는 올바른 <strong>위치 정보</strong>입니다.</v-alert>
+                                  <v-alert v-else dense outlined type="error">해당 <strong>주소 정보</strong>는 올바른 <strong>위치 정보</strong>가 아닙니다.</v-alert>
+                                  
+                                  <v-card-actions>
+                                    <v-spacer></v-spacer>
+                                        <v-btn v-if="rtrLocationCheck" color="success" text @click="rtrLocationdialog = false">확인</v-btn>
+                                        <v-btn v-else color="error" text @click="rtrLocationdialog = false">확인</v-btn>
+                                  </v-card-actions>
+                                </v-card>
+
+                            </v-dialog>
+                            
                         </v-card>
 
                         <v-divider class="mt-10 mb-10"></v-divider>
 
+
+                        <!--음식점 메뉴 - 반복문 -->
                         <v-card elevation="10" outlined v-for="menu,i in menulist" :key="i">
+                            
                             <v-app-bar flat color="">
                                 <v-spacer></v-spacer>
                                 <v-btn color="blue" text fab @click="addMenu()"><v-icon>mdi-plus</v-icon></v-btn>
                                 <v-btn color="blue" text fab @click="deleteMenu(i)"><v-icon>mdi-minus</v-icon></v-btn>
                             </v-app-bar>
+                            
                             <v-card-title class="text--primary font-weight-black">메뉴{{i+1}}</v-card-title>
+                            
                             <v-card-text>
                                 <ValidationProvider :rules="{
                                     required : true,
@@ -76,7 +116,7 @@
                             </v-card-text>
                         </v-card>
 
-                        <v-btn type="submit" block x-large rounded color="primary" class="mt-4" :disabled="invalid">등록하기</v-btn>
+                        <v-btn type="submit" block x-large rounded color="primary" class="mt-4" :disabled="invalid || !rtrLocationCheck" >등록하기</v-btn>
 
                     </v-form>
                 </ValidationObserver>
@@ -96,14 +136,39 @@ extend('required', {
   message : '해당 필드는 필수값입니다.'
 });
 
+extend('address', value => {
+
+    let is_break = false
+
+    const kakao = window.kakao
+    let geocoder = new kakao.maps.services.Geocoder();
+    geocoder.addressSearch(value, function(res,status){
+        if (!(status === kakao.maps.services.Status.OK)){
+    
+            console.log('error')
+            is_break = true
+        }else{
+            console.log(res[0].y, res[0].x)
+        }
+    });
+
+    if (is_break){
+        return '오류입니다.'
+    }
+    return true
+
+
+});
+
 export default {
     data(){
         return {
 
-            answer : null,
-
             rtrName: null,
+
             rtrLocation: null,
+            rtrLocationdialog : false,
+            rtrLocationCheck : false,
 
             menulist: [
                 {
@@ -112,6 +177,9 @@ export default {
                 },
             ],
         }
+    },
+
+    computed : {
     },
 
     components : {
@@ -154,10 +222,10 @@ export default {
             geocoder.addressSearch(this.rtrLocation, function(res,status){
                 if (status === kakao.maps.services.Status.OK){
 
-                    base.answer = `lat: ${res[0].y} lng: ${res[0].x}`
+                    base.rtrLocationCheck = true
                     console.log(res[0].y, res[0].x)
                 }else{
-                    base.answer = `못찾음`
+                    base.rtrLocationCheck = false
                     console.log('못찾음')
                 }
             });
@@ -167,7 +235,7 @@ export default {
             this.menulist.push({
                 menuName : null,
                 menuInfo : null,
-            });
+            }); 
         },
 
         deleteMenu(idx){
@@ -185,5 +253,11 @@ export default {
 </script>
 
 <style>
+.errorState {
+    background-color: red;
+}
 
+.successState {
+    background-color: blue;
+}
 </style>
