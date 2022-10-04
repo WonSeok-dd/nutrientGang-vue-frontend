eaeee219f9e370fcdda0b277d03f108ac566a400
WonSeok-dd <cluejws@gmail.com>
2022-10-04 17:53:47 +0900

    fix: 음식점 등록 기능 수정

--- src/layouts/register/RegisterRestaurant.vue
+++ src/layouts/register/RegisterRestaurant.vue
@@ -32,7 +32,7 @@
 
 
                         <!--음식점 주소-->
-                        <v-card elevation="10" outlined :class="{errorState : rtrLocationCheck === false, successState : rtrLocationCheck === true }">
+                        <v-card elevation="10" outlined>
                             <v-card-title class="text--primary font-weight-black">음식점 주소</v-card-title>
                             
                             <!--음식점 주소 입력-->
@@ -49,17 +49,18 @@
                             
 
                             <!--음식점 주소 다이얼로그 및 주소정보확인 버튼-->
+                            <!--
                             <v-dialog v-model="rtrLocationdialog" width="500">
                                 <template v-slot:activator="{ on, attrs }">
                                     <v-row justify="center" class="pd-5">
-                                        
-                                        <!--주소정보확인 메시지-->
+
+
                                         <v-col cols="auto">
                                             <v-alert v-if="rtrLocationCheck" dense outlined type="success">해당 주소 정보는 올바른 위치 정보입니다.</v-alert>
                                             <v-alert v-else dense outlined type="error">해당 주소 정보는 올바른 위치 정보가 아닙니다.</v-alert>
                                         </v-col>
-                                        
-                                        <!--주소정보확인 버튼-->
+
+
                                         <v-col cols="auto">
                                             <v-alert color="success" dark dense v-bind="attrs" v-on="on" @click="checkRtrLocation"> 
                                                 <v-icon left>mdi-map-marker-radius</v-icon>주소 정보 확인
@@ -68,20 +69,17 @@
                                     </v-row>
                                 </template>
 
-                                <!--음식점 주소 다이얼로그-->
                                 <v-card>
-                                  <v-alert v-if="rtrLocationCheck" dense outlined type="success">해당 <strong>주소 정보</strong>는 올바른 <strong>위치 정보</strong>입니다.</v-alert>
-                                  <v-alert v-else dense outlined type="error">해당 <strong>주소 정보</strong>는 올바른 <strong>위치 정보</strong>가 아닙니다.</v-alert>
-                                  
-                                  <v-card-actions>
-                                    <v-spacer></v-spacer>
+                                    <v-alert v-if="rtrLocationCheck" dense outlined type="success">해당 <strong>주소 정보</strong>는 올바른 <strong>위치 정보</strong>입니다.</v-alert>
+                                    <v-alert v-else dense outlined type="error">해당 <strong>주소 정보</strong>는 올바른 <strong>위치 정보</strong>가 아닙니다.</v-alert>
+
+                                    <v-card-actions>
                                         <v-btn v-if="rtrLocationCheck" color="success" text @click="rtrLocationdialog = false">확인</v-btn>
                                         <v-btn v-else color="error" text @click="rtrLocationdialog = false">확인</v-btn>
-                                  </v-card-actions>
+                                    </v-card-actions>
                                 </v-card>
-
                             </v-dialog>
-                            
+                            -->
                         </v-card>
 
                         <v-divider class="mt-10 mb-10"></v-divider>
@@ -116,13 +114,13 @@
                             </v-card-text>
                         </v-card>
 
-                        <v-btn type="submit" block x-large rounded color="primary" class="mt-4" :disabled="invalid || !rtrLocationCheck" >등록하기</v-btn>
+                        <v-btn type="submit" block x-large rounded color="primary" class="mt-4" :disabled="invalid" >등록하기</v-btn>
 
                     </v-form>
                 </ValidationObserver>
             </v-card-text>
-        </v-card>
 
+        </v-card>
     </v-container>
 
 </template>
@@ -135,27 +133,31 @@ extend('required', {
   ...required,
   message : '해당 필드는 필수값입니다.'
 });
-extend('address', value => {
-
-    let is_break = false
-
-    const kakao = window.kakao
-    let geocoder = new kakao.maps.services.Geocoder();
-    geocoder.addressSearch(value, function(res,status){
-        if (!(status === kakao.maps.services.Status.OK)){
+extend('address', async (value) => {
     
-            console.log('error')
-            is_break = true
-        }else{
-            console.log(res[0].y, res[0].x)
-        }
-    });
+    const kakao = window.kakao;
+    let geocoder = new kakao.maps.services.Geocoder();
 
-    if (is_break){
-        return '오류입니다.'
+    const addressSearch = address => {
+        return new Promise((resolve) => {
+            
+            geocoder.addressSearch(address, function(result, status) {
+                if (status === kakao.maps.services.Status.OK) {
+                    resolve(result);
+                } else {
+                    resolve(status)
+                }
+            });
+        });
+    };
+
+    const result = await addressSearch(value)
+    if (result === 'ZERO_RESULT'){
+        return '잘못된 주소 정보입니다.'
     }
     return true
 });
+
 
 export default {
     data(){
@@ -164,8 +166,8 @@ export default {
             rtrName: null,
 
             rtrLocation: null,
-            rtrLocationdialog : false,
-            rtrLocationCheck : false,
+            //rtrLocationdialog : false,
+            //rtrLocationCheck : false,
 
             menulist: [
                 {
@@ -208,24 +210,6 @@ export default {
             }
         },
         
-        //올바른 주소인지 확인 -> 버튼 클릭
-        checkRtrLocation(){
-            const kakao = window.kakao
-            let geocoder = new kakao.maps.services.Geocoder();
-
-            let base = this;
-            geocoder.addressSearch(this.rtrLocation, function(res,status){
-                if (status === kakao.maps.services.Status.OK){
-
-                    base.rtrLocationCheck = true
-                    console.log(res[0].y, res[0].x)
-                }else{
-                    base.rtrLocationCheck = false
-                    console.log('못찾음')
-                }
-            });
-        },
-
         //메뉴등록 칸 추가 -> 버튼 클릭
         addMenu(){
             this.menulist.push({
@@ -245,17 +229,25 @@ export default {
                 this.menulist.splice(idx,1)
             }
         },
+
+        //올바른 주소인지 확인 -> 버튼 클릭
+        //checkRtrLocation(){
+        //    const kakao = window.kakao
+        //    let geocoder = new kakao.maps.services.Geocoder();
+        //
+        //    let base = this;
+        //    geocoder.addressSearch(this.rtrLocation, function(res,status){
+        //        if (status === kakao.maps.services.Status.OK){
+        //
+        //            base.rtrLocationCheck = true
+        //            console.log(res[0].y, res[0].x)
+        //        }else{
+        //            base.rtrLocationCheck = false
+        //            console.log('못찾음')
+        //        }
+        //    });
+        //},
     },
 
 }
 </script>
-
-<style>
-.errorState {
-    background-color: red;
-}
-
-.successState {
-    background-color: blue;
-}
-</style>
