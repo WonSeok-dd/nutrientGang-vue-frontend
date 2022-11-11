c0b4094cb38f045b5c53c7d09c7e77ed2e418373
WonSeok-dd <cluejws@gmail.com>
2022-11-12 06:58:23 +0900

    fix: ReportBalanceBarChart의 Report API 완성

--- src/components/Report/ReportBalance/ReportBalanceBarChart.vue
+++ src/components/Report/ReportBalance/ReportBalanceBarChart.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="fill-height mt-10" v-if="isError">
+  <div class="fill-height mt-10" v-if="isBarChartError">
       <v-row justify="center">
           <v-col cols="auto">
               <ServerErrorComponent/>
@@ -8,285 +8,26 @@
       </v-row>
   </div>
   <div class="text-center mt-10 border" v-else>
-    <BarChartGenerator
-      :chart-options="chartOptions"
-      :chart-data="chartData"
-    />
+    <BarChartGenerator :chart-options="barChartOption" :chart-data="barChartData"/>
   </div>
 
 </template>
 
 <script>
 const ServerErrorComponent = () => import("@/components/ServerErrorComponent.vue");
-import Report from '@/api/Report'
 import { Bar as BarChartGenerator } from 'vue-chartjs/legacy'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale} from 'chart.js'
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ChartDataLabels)
 
 export default {
   name : 'ReportBalanceBarChart',
   props : {
-    dates : {
-        type : Array,
-        default : () => [
-          new Date(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).setDate(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).getDate() - 6)).toISOString().substr(0,10), 
-          new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000).toISOString().substr(0,10)
-        ]
-    },
+      barChartData : {type : Object},
+      barChartOption : {type: Object},
+      isBarChartError : {type: Boolean}
   },
   components: {
     BarChartGenerator,
     "ServerErrorComponent" : ServerErrorComponent
   },
-
-  watch : {
-    dates : {
-      immediate : true,
-      handler(dates){
-        
-        const begin = dates[0];
-        const end = dates[1];
-
-        //중요) 필요한 데이터 요청
-        Report.getBalanceGraph(begin,end)
-        .then((res) =>{
-            console.log(res.data.message);
-            if(res.data.isSuccess === true && res.data.code === 1000){
-                //중요) 요청에 성공하였습니다.
-                const weekPortionList = res.data.result.weekPortionList;
-                const reverseList = [...weekPortionList].reverse();
-
-                let portionLabels = [];
-                let portionCarboData = [];
-                let portionProteinData = [];
-                let portionFatData = [];
-                for (const weekPortion of reverseList){
-                  
-                  let date = weekPortion.date;
-                  let dateArray = date.split('-');
-                  const finalDate = dateArray[1] + '.' + dateArray[2];
-                  portionLabels.push(finalDate);
-
-                  let carbohydratePortion = weekPortion.carbohydratePortion;
-                  const finalCarbohydratePortion = carbohydratePortion * 100;
-
-                  let proteinPortion = weekPortion.proteinPortion;
-                  const finalProteinPortion = proteinPortion * 100;
-
-                  let fatPortion = weekPortion.fatPortion;
-                  const finalFatPortion = fatPortion * 100;
-
-                  portionCarboData.push(finalCarbohydratePortion);
-                  portionProteinData.push(finalProteinPortion);
-                  portionFatData.push(finalFatPortion);
-                }
-
-                this.chartData.labels = portionLabels;
-                this.chartData.datasets[0].data = portionFatData;
-                this.chartData.datasets[1].data = portionProteinData;
-                this.chartData.datasets[2].data = portionCarboData;
-
-            }else if (res.data.isSuccess === false && res.data.code === "NO_AUTHORIZATION"){
-                //중요) 인증 정보 없으니까 로그아웃 후 리다이렉션
-                //돌리기 -> 하지만 이미 레이아웃이 그려지기 전에 이미 재발행 받아서 로그인 페이지로 돌려지지 않음
-                this.$store.dispatch('logout');
-                this.$router.push({
-                    name : "sign-in",
-                });
-            }else{
-                //중요) 건강정보를 찾을 수 없습니다.
-                const begin = this.dates[0];
-                let dateLabels = [];
-                for (let i=0; i<7; i++){
-                  let tempDate = new Date(begin);
-                  tempDate.setDate(tempDate.getDate()+ i);
-
-                  let strDate = tempDate.toISOString().substring(0,10);
-                  let strDateArray = strDate.split('-');
-                  const finalDate = strDateArray[1] + '.' + strDateArray[2];
-
-                  dateLabels.push(finalDate);
-                }
-
-                this.chartData.labels = dateLabels;
-            }
-        })
-        .catch((err)=>{
-            //중요) 서버 오류입니다.
-            //뜨기 -> alert메시지 뜨기
-            console.log(err);
-            this.isError = true;
-        })
-      }
-    }
-  },
-
-  data() {
-    return {
-
-      //에러 판단
-      isError : false,
-
-      chartData: {
-        labels: ['null','null','null','null','null','null','null'],
-        datasets: [
-          {
-            label: '지방',
-            stack: 'combined',
-            type : 'bar',
-            backgroundColor: 'rgb(255, 205, 86)',
-            data: [0,0,0,0,0,0,0],
-            datalabels: {
-                display : false,
-            },
-          },
-          {
-            label: '단백질',
-            stack: 'combined',
-            type : 'bar',
-            backgroundColor: 'rgb(54, 162, 235)',
-            data: [0,0,0,0,0,0,0],
-            datalabels: {
-                display : false,
-            },
-          },
-          {
-            label: '탄수화물',
-            stack: 'combined',
-            type : 'bar',
-            backgroundColor: 'rgb(255, 99, 132)',
-            data: [0,0,0,0,0,0,0],
-            datalabels: {
-                display : false,
-            },
-          },
-        ]
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        animation: {
-          duration: 5000,
-        },
-        scales: {
-            x: {
-              stacked: true,
-              display: true,
-
-              //x축 label 설정
-              ticks: {
-                padding: 10,
-                font : {
-                  family : 'Jua',
-                  size : 10,
-                },
-              },
-
-              grid: {
-                display : true,         //이건 default
-                drawOnChartArea : true, //이건 default
-                drawBorder : true,      
-                drawTicks : false,      
-              },
-
-            },
-            y: {
-              stacked: true,
-              display : true,
-              
-              //y축 label 설정
-              ticks: {
-                display : true,
-                stepSize : 5, 
-                font : {
-                  family : 'Jua',
-                  size : 10,
-                },
-
-                callback : (value) =>{
-                  if(value === 25){
-                    return '25%'
-                  }else if(value === 50){
-                    return '50%'
-                  }
-                }
-              },
-
-              grid : {
-                display : true,         //이건 default
-                drawOnChartArea : true, //이건 default
-                drawBorder : true,      
-                drawTicks : true,      
-                color: function(context) {
-                
-                  if (context.tick.value === 25) {
-                    return 'rgb(255, 205, 86)';
-                  }else if(context.tick.value === 50) {
-                    return 'rgb(255, 99, 132)';
-                  }
-                },
-              }
-
-            }
-        },
-
-        //그래프 특징
-        plugins: {
-
-            //그래프 종류 표시
-            legend: {              
-                display: true,
-                align : 'end',
-                fullSize : false,
-                labels :{
-                    boxWidth : 10,
-                    font : {
-                      family : 'Jua',
-                      size : 13,
-                    },
-                },
-
-                onClick: () => {
-                  //pass
-                },
-            },
-
-            //그래프 점 표시
-            tooltip: {
-                callbacks : {
-                    //매개변수 -> return 변화 매개변수 
-                    label : (tooltipItem) => {
-                        const text = tooltipItem.dataset.label + '(' + tooltipItem.formattedValue + '%)';
-                        return text;
-                    }
-                },
-                boxWidth: 10,
-                titleFont: {
-                    size: 14,
-                    family : 'Jua'
-                },
-                bodyFont : {
-                    size: 13,
-                    family : 'Jua'
-                },
-            },
-
-            //그래프 제목 표시
-            title : {
-                display : true,
-                font : {
-                    family : 'Jua',
-                    size : 20
-                },
-                text : '일주일 간 섭취 비율(%)'
-            },
-        },
-        
-      }
-    }
-  }
 }
 </script>
 <style  scoped>
--- src/layouts/MyPage/Report/ReportBalance.vue
+++ src/layouts/MyPage/Report/ReportBalance.vue
@@ -51,7 +51,8 @@
 
     <!--날짜별 영양소 막대 차트-->
     <div class="mb-3">
-      <ReportBalanceBarChart :dates="dates"/>
+      <ReportBalanceBarChart :barChartData="updateBarChartData" :barChartOption="updateBarChartOption"
+      :isBarChartError="isBarChartError"/>
     </div>
 
   </div>
@@ -59,11 +60,11 @@
 
 <script>
 import Report from '@/api/Report';
-import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale} from 'chart.js'
+import { Chart as ChartJS, Title, Tooltip, Legend, ArcElement, CategoryScale, BarElement, LinearScale} from 'chart.js'
 import chartjsPluginDoughnutlabel from "chartjs-plugin-doughnutlabel-rebourne";
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 
-ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale, chartjsPluginDoughnutlabel, ChartDataLabels)
+ChartJS.register(Title, Tooltip, Legend, BarElement, LinearScale, ArcElement, CategoryScale, chartjsPluginDoughnutlabel, ChartDataLabels)
 
 const ReportBalancePieChart = () => import("@/components/Report/ReportBalance/ReportBalancePieChart.vue");
 const ReportBalanceNutrient = () => import("@/components/Report/ReportBalance/ReportBalanceNutrient.vue");
@@ -96,6 +97,7 @@ export default {
         const end = dates[1];
 
         //파이차트
+        //중요) 필요한 데이터 요청
         Report.getBalancePortion(begin, end)
         .then((res) =>{
             console.log(res.data.message);
@@ -104,6 +106,7 @@ export default {
                 const carboPortion = (res.data.result.carbohydratePortion * 100);
                 const proteinPortion = (res.data.result.proteinPortion * 100);
                 const fatPortion = (res.data.result.fatPortion * 100);
+                
                 this.fillPieChartData(carboPortion, proteinPortion, fatPortion);
             }else if (res.data.isSuccess === false && res.data.code === "NO_AUTHORIZATION"){
                 //중요) 인증 정보 없으니까 로그아웃 후 리다이렉션
@@ -125,6 +128,80 @@ export default {
         });
 
         //바 차트
+        //중요) 필요한 데이터 요청
+        Report.getBalanceGraph(begin,end)
+        .then((res) =>{
+            console.log(res.data.message);
+            if(res.data.isSuccess === true && res.data.code === 1000){
+                //중요) 요청에 성공하였습니다.
+                const weekPortionList = res.data.result.weekPortionList;
+                const reverseList = [...weekPortionList].reverse();
+
+                let portionLabels = [];
+                let portionCarboData = [];
+                let portionProteinData = [];
+                let portionFatData = [];
+                for (const weekPortion of reverseList){
+                  
+                  let date = weekPortion.date;
+                  let dateArray = date.split('-');
+                  const finalDate = dateArray[1] + '.' + dateArray[2];
+                  portionLabels.push(finalDate);
+
+                  let carbohydratePortion = weekPortion.carbohydratePortion;
+                  const finalCarbohydratePortion = carbohydratePortion * 100;
+
+                  let proteinPortion = weekPortion.proteinPortion;
+                  const finalProteinPortion = proteinPortion * 100;
+
+                  let fatPortion = weekPortion.fatPortion;
+                  const finalFatPortion = fatPortion * 100;
+
+                  portionCarboData.push(finalCarbohydratePortion);
+                  portionProteinData.push(finalProteinPortion);
+                  portionFatData.push(finalFatPortion);
+                }
+
+                this.fillBarChartLabels(portionLabels);
+                this.fillBarChartFatData(portionFatData);
+                this.fillBarChartProteinData(portionProteinData);
+                this.fillBarChartCarboData(portionCarboData);
+
+            }else if (res.data.isSuccess === false && res.data.code === "NO_AUTHORIZATION"){
+                //중요) 인증 정보 없으니까 로그아웃 후 리다이렉션
+                //돌리기 -> 하지만 이미 레이아웃이 그려지기 전에 이미 재발행 받아서 로그인 페이지로 돌려지지 않음
+                this.$store.dispatch('logout');
+                this.$router.push({
+                    name : "sign-in",
+                });
+            }else{
+                //중요) 건강정보를 찾을 수 없습니다.
+                const begin = this.dates[0];
+                let dateLabels = [];
+                for (let i=0; i<7; i++){
+                  let tempDate = new Date(begin);
+                  tempDate.setDate(tempDate.getDate()+ i);
+
+                  let strDate = tempDate.toISOString().substring(0,10);
+                  let strDateArray = strDate.split('-');
+                  const finalDate = strDateArray[1] + '.' + strDateArray[2];
+
+                  dateLabels.push(finalDate);
+                  
+                }
+
+                this.fillBarChartLabels(dateLabels);
+                this.fillBarChartFatData([0,0,0,0,0,0,0]);
+                this.fillBarChartProteinData([0,0,0,0,0,0,0]);
+                this.fillBarChartCarboData([0,0,0,0,0,0,0]);
+            }
+        })
+        .catch((err)=>{
+            //중요) 서버 오류입니다.
+            //뜨기 -> alert메시지 뜨기
+            console.log(err);
+            this.isBarChartError = true;
+        })
       }
     }
   },
@@ -135,6 +212,12 @@ export default {
 
         isPieChartError : false,
         pieChartData : [0,0,0],
+
+        isBarChartError : false,
+        barChartLabel : ['null','null','null','null','null','null','null'],
+        barChartCarboData : [0,0,0,0,0,0,0],
+        barChartProteinData : [0,0,0,0,0,0,0],
+        barChartFatData : [0,0,0,0,0,0,0],
 
         dateDialog: false,
     }
@@ -226,7 +309,7 @@ export default {
                         family : 'Jua',
                         size : 20
                     },
-                    text : '일주일 섭취 영양소 비율(%)'
+                    text : '일주일 총 섭취 영양소 비율(%)'
                 },
 
                 //그래프 안 제목 표시
@@ -251,6 +334,174 @@ export default {
                 },
             },
       }
+
+      return chartOptions;
+    },
+
+    updateBarChartData(){
+      let chartData = {
+        
+        labels: this.barChartLabel,
+        datasets: [
+          {
+            label: '지방',
+            stack: 'combined',
+            type : 'bar',
+            backgroundColor: 'rgb(255, 205, 86)',
+            data: this.barChartFatData,
+            datalabels: {
+                display : false,
+            },
+          },
+          {
+            label: '단백질',
+            stack: 'combined',
+            type : 'bar',
+            backgroundColor: 'rgb(54, 162, 235)',
+            data: this.barChartProteinData,
+            datalabels: {
+                display : false,
+            },
+          },
+          {
+            label: '탄수화물',
+            stack: 'combined',
+            type : 'bar',
+            backgroundColor: 'rgb(255, 99, 132)',
+            data: this.barChartCarboData,
+            datalabels: {
+                display : false,
+            },
+          }]
+      }
+
+      return chartData;
+    },
+
+    updateBarChartOption(){
+
+      let chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: {
+          duration: 5000,
+        },
+        scales: {
+            x: {
+              stacked: true,
+              display: true,
+
+              //x축 label 설정
+              ticks: {
+                padding: 10,
+                font : {
+                  family : 'Jua',
+                  size : 10,
+                },
+              },
+
+              grid: {
+                display : true,         //이건 default
+                drawOnChartArea : true, //이건 default
+                drawBorder : true,      
+                drawTicks : false,      
+              },
+
+            },
+            y: {
+              stacked: true,
+              display : true,
+              
+              //y축 label 설정
+              ticks: {
+                display : true,
+                stepSize : 5, 
+                font : {
+                  family : 'Jua',
+                  size : 10,
+                },
+
+                callback : (value) =>{
+                  if(value === 25){
+                    return '25%'
+                  }else if(value === 50){
+                    return '50%'
+                  }
+                }
+              },
+
+              grid : {
+                display : true,         //이건 default
+                drawOnChartArea : true, //이건 default
+                drawBorder : true,      
+                drawTicks : true,      
+                color: function(context) {
+                
+                  if (context.tick.value === 25) {
+                    return 'rgb(255, 205, 86)';
+                  }else if(context.tick.value === 50) {
+                    return 'rgb(255, 99, 132)';
+                  }
+                },
+              }
+
+            }
+        },
+
+        //그래프 특징
+        plugins: {
+
+            //그래프 종류 표시
+            legend: {              
+                display: true,
+                align : 'end',
+                fullSize : false,
+                labels :{
+                    boxWidth : 10,
+                    font : {
+                      family : 'Jua',
+                      size : 13,
+                    },
+                },
+
+                onClick: () => {
+                  //pass
+                },
+            },
+
+            //그래프 점 표시
+            tooltip: {
+                callbacks : {
+                    //매개변수 -> return 변화 매개변수 
+                    label : (tooltipItem) => {
+                        const text = tooltipItem.dataset.label + '(' + tooltipItem.formattedValue + '%)';
+                        return text;
+                    }
+                },
+                boxWidth: 10,
+                titleFont: {
+                    size: 14,
+                    family : 'Jua'
+                },
+                bodyFont : {
+                    size: 13,
+                    family : 'Jua'
+                },
+            },
+
+            //그래프 제목 표시
+            title : {
+                display : true,
+                font : {
+                    family : 'Jua',
+                    size : 20
+                },
+                text : '일주일 간 섭취 비율(%)'
+            },
+        },
+
+
+      };
 
       return chartOptions;
     },
@@ -281,6 +532,22 @@ export default {
 
       fillPieChartData(carboPortion, proteinPortion, fatPortion){
         this.pieChartData = [carboPortion, proteinPortion, fatPortion];
+      },
+
+      fillBarChartLabels(portionLabels){
+        this.barChartLabel = portionLabels;
+      },
+
+      fillBarChartCarboData(portionCarboData){
+        this.barChartCarboData = portionCarboData;
+      },
+
+      fillBarChartProteinData(portionProteinData){
+        this.barChartProteinData = portionProteinData;
+      },
+
+      fillBarChartFatData(portionFatData){
+        this.barChartFatData = portionFatData;
       },
 
       //computed 통해 날짜 할당시 format
